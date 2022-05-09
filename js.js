6ef93b95747236b3383c265b0654caffd43165f3
Junwei <junwei.he.99@gmail.com>
2022-05-09 13:14:41 -0700

    Added styling, borders for invalid entries, and password validation

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
+const password = document.querySelector('#password');
+const confirm_passsword = document.querySelector('#confirm_password');
 
+function validatePassword(){
+    if(password.value != confirm_passsword.value){
+        confirm_passsword.setCustomValidity("Passwords Don't Match");
+    }
+    else{
+        confirm_passsword.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_passsword.onkeyup = validatePassword;
